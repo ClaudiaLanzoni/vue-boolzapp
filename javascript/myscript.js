8e7f5870053bb1c0a4ad2f04bb2857fdf8a7133f
ClaudiaLanzoni <claudia.lanzoni1991@gmail.com>
2021-09-22 19:27:13 +0200

    comments on myscript, grafica migliorata

--- javascript/myscript.js
+++ javascript/myscript.js
@@ -93,13 +93,13 @@ let app = new Vue ({
         selectedContact: 0,
 
         inputLine: {
-            date : new Date,
+            date : "22/09/2021 15:30:55",
             text: "",
             status : 'sent'
         },
 
         automaticReply : {
-            date : new Date(),
+            date : "22/09/2021 15:30:56",
             text : "ok",
             status : 'received'
         },
@@ -109,16 +109,20 @@ let app = new Vue ({
 
     methods: {
 
+        //metodo per il click sulla chat attiva che permette di selezionare l'indice "corrente"
         selectContact(elementIndex) {
             this.selectedContact = elementIndex;
         },
 
+        //metodo per selezionare l'indice dell'ultimo messaggio inviato
         renderLastElement(index) {
             let contactMessages = this.contacts[index].messages;
+            
             return contactMessages[contactMessages.length -1]
 
         },
 
+        //metodo per aggiungere il messaggio input dell'utente nell'array messaggi
         addTextToChat() {
             if (this.inputLine.text.trim('').length > 0) {
                 this.contacts[this.selectedContact].messages.push(this.inputLine);
@@ -126,21 +130,35 @@ let app = new Vue ({
             }
         },
 
+        //metodo per aggiungere risposta automatica in seguito al messaggio input dell'utente
         createReplyToChat() {
             if (this.contacts[this.selectedContact].messages.push(this.inputLine)) {
             this.contacts[this.selectedContact].messages.push(this.automaticReply)
             }
         },
 
+        //funzione che richiama la funzione della risposta automatica e la fa partire dopo 1 secondo
         addReplyToChat() {
             setTimeout(this.createReplyToChat, 1000);
         },   
     },
 
-    computed : {
+    computed : { //property: data that needs to change based on some other data
         
+        /**
+         * il filterScript è un array "aggiornato" in base all'inputContactSearch(cioè l'input utente),
+         * dove tutto ciò che non corrisponde all'input utente (nella proprietà richiesta, in questo caso .name)
+         * viene "filtrato via"
+         * 
+         * @returns prima di tutto il current array (this.contacts), a cui si applica il filtro che
+         * a ciascun ciclo ritornerà il "valore" (input); 
+         * l'arrow function viene performata ogni giro del ciclo per ogni item "valore" (input);
+         * viene specificata la proprietà dentro cui controllare (attraverso match() ) l'input;
+         * in caso ritorni "true" mantiene questo "valore" nell'array filtrato;
+         */
+
         filterScript: function(){
-            return this.contacts.filter((input) => {
+            return this.contacts.filter((input) => { 
                 return input.name.toLowerCase().match(this.inputContactSearch);
             });
         }
